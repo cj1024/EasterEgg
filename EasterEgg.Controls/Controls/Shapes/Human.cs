725fcae1da30696a74505887be7f19bc0d9fa5bc
Jian <ferdinand31@sohu.com>
2014-07-24 15:37:25 +0800

    Remove Useless NameSpace Using

--- EasterEgg.Controls/Controls/Shapes/Human.cs
+++ EasterEgg.Controls/Controls/Shapes/Human.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace EasterEgg.Controls
